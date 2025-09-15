fb31f85a1d76b0ec16ea98cebdddddf04541e1ab
anshitkumarsharma <hianshithere@yahoo.co.in>
2025-09-15 21:18:11 +0530

    Add configuration class for database setup; enable transaction management and aspect-oriented programming

--- src/main/java/org/example/config/ProductConfig.java
+++ src/main/java/org/example/config/ProductConfig.java
@@ -10,10 +10,13 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.TransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-//@Configuration
+/**
+ * DBConfig class
+ *
+ * @author Anshit
+ */
+
 @ComponentScan("org.example")
-// Transational was implemented but to enable rollback feature we should have something of a
-// basic config: EnableTransactionManagement
 @EnableTransactionManagement
 @EnableAspectJAutoProxy
 public class ProductConfig {
@@ -21,11 +24,13 @@ public class ProductConfig {
 
     @Bean
     public JdbcTemplate getJdbcTemplate() {
+
         return new JdbcTemplate(getDatasource());
     }
 
     @Bean
     public DataSource getDatasource() {
+
         return new DriverManagerDataSource(
                 "jdbc:mysql://localhost:3306/springTransactions",
                 "root",
@@ -34,6 +39,7 @@ public class ProductConfig {
 
     @Bean
     public TransactionManager getTransactionManager() {
+
         return new DataSourceTransactionManager(getDatasource());
     }
 }
